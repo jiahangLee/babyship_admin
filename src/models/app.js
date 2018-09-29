3b8f29d71c2244e7171cf2decabd31822155cf61
jiahanglee <1191039404@qq.com>
2018-09-29 23:46:54 +0800

    a little

--- src/models/app.js
+++ src/models/app.js
@@ -1,8 +1,9 @@
-import { routerRedux } from 'dva/router'
-import {parse}  from "qs";
+import {routerRedux} from 'dva/router'
+import {parse} from "qs";
 import queryString from 'query-string'
 import config from '../utils/config'
-import {query,logout} from '../services/app'
+import {query, logout} from '../services/app'
+
 export default {
 
   namespace: 'app',
@@ -14,23 +15,24 @@ export default {
   },
 
   subscriptions: {
-    setupHistory ({dispatch,history}){
-    history.listen((location => {
-    dispatch({
-      type: 'updateState',
-      payload: {
-        locationPathname: location.pathname,
-        locationQuery: location.query,
-      },
-    })
-  }))},
-    setup({ dispatch, pathname }) {
-      dispatch({type: 'start',payload:pathname})
+    setupHistory({dispatch, history}) {
+      history.listen((location => {
+        dispatch({
+          type: 'updateState',
+          payload: {
+            locationPathname: location.pathname,
+            locationQuery: location.query,
+          },
+        })
+      }))
+    },
+    setup({dispatch, pathname}) {
+      dispatch({type: 'start', payload: pathname})
       let tid
       window.onresize = () => {
         clearTimeout(tid)
         tid = setTimeout(() => {
-          dispatch({ type: 'changeNavbar' })
+          dispatch({type: 'changeNavbar'})
         }, 300)
       }
 
@@ -38,19 +40,24 @@ export default {
   },
 
   effects: {
-    *start({payload},{select,put,call }){
+    * start({payload}, {select, put, call}) {
       const {data} = yield call(query, payload)
       console.log(JSON.stringify(data));
-      const { locationPathname } = yield select(_ => _.app)
+      const {locationPathname} = yield select(_ => _.app)
       console.log("ok")
-      if (data.status==="success") {
-        console.log("******************"+payload)
-        //这里没有payload
-        if (payload === '/login') {
-          yield put(routerRedux.push({
-            pathname: '/news',
-          }))
-        }
+      if (data) {
+        if (data.status === "success") {
+          console.log("******************" + payload)
+          //这里没有payload
+          if (payload === '/login') {
+            yield put(routerRedux.push({
+              pathname: '/news',
+            }))}
+          }else if(data.status===500){
+            yield put(routerRedux.push({
+              pathname: '/login',
+            }))
+          }
       } else if (config.openPages && config.openPages.indexOf(locationPathname) < 0) {
         yield put(routerRedux.push({
           pathname: '/login',
@@ -60,28 +67,30 @@ export default {
         }))
       }
     },
-    *fetch({ payload }, { call, put }) {
-      yield put({ type: 'save' });
+    * fetch({payload}, {call, put}) {
+      yield put({type: 'save'});
     },
-    *fetchCname({ payload }, { call, put }) {
-      yield put({ type: 'save' });
+    * fetchCname({payload}, {call, put}) {
+      yield put({type: 'save'});
     },
-    * changeNavbar (action, { put, select }) {
-      const { app } = yield (select(_ => _))
+    * changeNavbar(action, {put, select}) {
+      const {app} = yield (select(_ => _))
       const isNavbar = document.body.clientWidth < 769
       if (isNavbar !== app.isNavbar) {
-        yield put({ type: 'handleNavbar', payload: isNavbar })
+        yield put({type: 'handleNavbar', payload: isNavbar})
       }
     },
-    * logout ({
-                payload,
-              }, { call, put }) {
+    * logout({
+               payload,
+             }, {call, put}) {
       const data = yield call(logout, parse(payload))
       if (data.success) {
-        yield put({ type: 'updateState', payload: {
+        yield put({
+          type: 'updateState', payload: {
             user: {},
-          }})
-        yield put({ type: 'query' })
+          }
+        })
+        yield put({type: 'query'})
       } else {
         throw (data)
       }
@@ -89,14 +98,14 @@ export default {
   },
 
   reducers: {
-    updateState (state, { payload }) {
+    updateState(state, {payload}) {
       return {
         ...state,
         ...payload,
       }
     },
     save(state, action) {
-      return { ...state, ...action.payload };
+      return {...state, ...action.payload};
     },
   },
 };
