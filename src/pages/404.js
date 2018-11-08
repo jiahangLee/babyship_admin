4e42ce738a9746e5863ef046121083bb07696e65
jiahanglee <1191039404@qq.com>
2018-11-08 17:51:12 +0800

    404统一处理，配合hashHistory使用

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { connect } from 'dva';
 import styles from './index.css';
 
-function IndexPage() {
+export default () => {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>***** 正在建设! *****</h1>
+      <h1 className={styles.title}>***** 你想要干嘛？ *****</h1>
       <div className={styles.welcome} />
       <ul className={styles.list}>
       </ul>
     </div>
   );
-}
-
-IndexPage.propTypes = {
 };
-
-export default connect()(IndexPage);
